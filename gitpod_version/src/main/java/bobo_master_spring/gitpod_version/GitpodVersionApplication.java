e33bcec45bccd242a4673884ad0bbbbd49a2fa14
Boris Atud <borisatud@icloud.com>
2025-05-04 15:42:56 +0000

    setup for mavan spring boot project.

---
+++ gitpod_version/src/main/java/bobo_master_spring/gitpod_version/GitpodVersionApplication.java
@@ -0,0 +1,13 @@
+package bobo_master_spring.gitpod_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpodVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpodVersionApplication.class, args);
+	}
+
+}
---
+++ gitpod_version/src/test/java/bobo_master_spring/gitpod_version/GitpodVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package bobo_master_spring.gitpod_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpodVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
