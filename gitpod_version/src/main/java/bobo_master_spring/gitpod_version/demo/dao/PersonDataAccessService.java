aa76a80afb0a9f79cae81df75e7e03650331b7ae
Boris Atud <borisatud@icloud.com>
2025-05-05 13:06:39 +0000

    Add code to process selectAllData

--- gitpod_version/src/main/java/bobo_master_spring/gitpod_version/demo/dao/PersonDataAccessService.java
+++ gitpod_version/src/main/java/bobo_master_spring/gitpod_version/demo/dao/PersonDataAccessService.java
@@ -1,15 +1,26 @@
 package bobo_master_spring.gitpod_version.demo.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import bobo_master_spring.gitpod_version.demo.model.Person;
 
 @Repository("postgress")
 public class PersonDataAccessService implements PersonDao{
+    
+    private final JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public int insertPerson(UUID id, Person person) {
@@ -19,8 +30,24 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM postgress DB"));
+        String sqlCommand = "SELECT id, name FROM persons";
+        List<Person> people = new ArrayList<>();
+        try {
+            people = jdbcTemplate.query(sqlCommand, personInfo -> {
+                UUID id = UUID.fromString(personInfo.getString("id"));
+                String name = personInfo.getString("name");
+                return new Person(id, name);
+            });
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return people;
     }
+    // @Override
+    // public List<Person> selectAllPeople() {
+    //     return List.of(new Person(UUID.randomUUID(), "This is for PG"));
+    // }
 
     @Override
     public int deletePersonbyId(UUID id) {
