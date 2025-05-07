1b16e27261d9321fe290a4531d035a824e49fc25
Boris Atud <borisatud@icloud.com>
2025-05-07 02:34:37 +0000

    didn't work

--- init.sql
+++ init.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE springbootdb;
 \c springbootdb;
 CREATE EXTENSION "uuid-ossp";
-INSERT INTO person (id, name) VALUES (uuid_generate_v4(), 'Paul Randall');
-INSERT INTO person (id, name) VALUES (uuid_generate_v4(), 'John Just');
